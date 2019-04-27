bc6fa7629548042f6eeeee450adf59060a9beec2
MSobiya <sobiyasardar@gmail.com>
2019-04-27 17:40:43 +0530

    Update Connectivity.php

--- Web/Connectivity.php
+++ Web/Connectivity.php
@@ -56,47 +56,6 @@ else
 }
 
 
-//---------------------------------------------------Send OTP-----------------------------------------------
-// if(isset($_POST['btn_otp'])){
-// session_start();
-// require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
-// require '/usr/share/php/libphp-phpmailer/class.smtp.php';
-// $rndno=rand(1000, 9999);
-// $mail = new PHPMailer(); // create a new object
-// $mail->IsSMTP(); // enable SMTP
-// //$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-// $mail->SMTPAuth = true; // authentication enabled
-// $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-// $mail->Host = "smtp.gmail.com";
-// $mail->Port = 465; // or 587
-// $mail->IsHTML(true);
-// $mail->Username = "farhatsardar9@gmail.com";
-// $mail->Password = "sobiya786";
-// $mail->SetFrom("farhatsardar9@gmail.com");
-// $mail->Subject = "OTP";
-// $mail->Body = $rndno;
-// $mail->AddAddress($_POST['email']);
-
-// if(!$mail->Send()) {
-//     echo "Mailer Error: " . $mail->ErrorInfo;
-//  } else {
-//     echo "Message has been sent";
-//     $_SESSION['email']=$_POST['email'];
-//     $_SESSION['otp']=$rndno;
-    
-
-//     //echo "hideAllTabs();";
-//     echo "run();";
-//     header( "Location: Register.php");
-
-//      //header("Location: Register.php#verification");	
-    
-//     //header("Location: ".$config['Register.php']."Register.php#verification");
-//  }
-
-// }
-
-
 
 
 
